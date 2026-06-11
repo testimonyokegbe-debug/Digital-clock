25b8fcd2ba3ab0b749af8a7152eb9a759ee023bf
Iruoghene <testimonyokegbe@gmail.com>
2026-06-11 05:19:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-let hrs= document.getElementById("hrs")
-let min= document.getElementById("min")
-let sec= document.getElementById("sec")
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+let dateEl = document.getElementById("date");
 
-setInterval(()=>{
+setInterval(() => {
+    let currentTime = new Date();
 
+    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 
-let currentTime=new Date();
+    // NICE HUMAN DATE FORMAT 👇
+    let options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
 
-hrs.innerHTML = (currentTime.getHours()<10?"0":"")+currentTime.getHours();
-min.innerHTML = (currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
-sec.innerHTML = (currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
-},1000)
+    dateEl.innerHTML = currentTime.toLocaleDateString("en-US", options);
+
+}, 1000);
